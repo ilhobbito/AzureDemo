9383fa4493b35ad592c3a470db28af7bcb682f44
Robin Jonsson <jonsson_robin@icloud.com>
2024-11-18 11:35:28 +0100

    Update

--- obj/Debug/net9.0/AzureDemo.AssemblyInfo.cs
+++ obj/Debug/net9.0/AzureDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da990f52c9b5f6a51c19f5204594f01a66173216")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
